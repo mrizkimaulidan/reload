a48df52c7444342d4aeedbaa1085ae8b76505f81
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2021-07-29 03:34:13 +0800

    improvements

--- script.py
+++ script.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import requests
 
-url = input('URL (http/https) : ')
+print('1. Reload using webdriver browser\n2. Reload with request get')
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+options = int(input('Choose your options : '))
 
-browser.get(url)
+if options == 1:
+    print('Option %s selected' % options)
+    url = input('URL (http/https) : ')
 
-while True:
-    browser.refresh()
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    browser.get(url)
+
+    while True:
+        browser.refresh()
+
+else:
+    print('Option %s selected' % options)
+    url = input('URL (http/https) : ')
+
+    while True:
+        requests.get(url)
